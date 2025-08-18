e790e6c9e0e929a0c868352d4fff635648f305f6
Baruch Yankovitz <baruchyankovitz@gmail.com>
2025-08-18 11:01:26 +0300

    fix routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,34 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
+import { UserDashboardComponent } from './components/user-dashboard/user-dashboard'; 
+import { UserManagementComponent } from './components/userMang/userMang.component'; 
+import { OrdersComponent } from './components/orders/orders.component';
+import { SitesComponent } from './components/sites/sites.component';
+import { AuthGuard } from './guards/auth.guard';
+import { ManagerGuard } from './guards/manager.guard';
 
 export const routes: Routes = [
-    { path: '', redirectTo: '/login', pathMatch: 'full' },
+    { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
     { path: 'login', component: LoginComponent },
+    { 
+        path: 'dashboard', 
+        component: UserDashboardComponent, 
+        canActivate: [AuthGuard] 
+    },
+    { 
+        path: 'user-management', 
+        component: UserManagementComponent, 
+        canActivate: [AuthGuard, ManagerGuard] 
+    },
+    { 
+        path: 'orders', 
+        component: OrdersComponent, 
+        canActivate: [AuthGuard] 
+    },
+    { 
+        path: 'sites', 
+        component: SitesComponent, 
+        canActivate: [AuthGuard] 
+    },
+    { path: '**', redirectTo: '/dashboard' } 
 ];
